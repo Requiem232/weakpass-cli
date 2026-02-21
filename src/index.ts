5a968d40748f68b33d11186ab366116181fa34c2
ibnaleem <ibnaleem@outlook.com>
2026-02-21 15:05:07 +0000

    feat: load search command for weakpass-cli

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
+import { loadCommands } from './commands/search';
 
 const program = new Command();
 
@@ -7,4 +8,5 @@ program.name('weakpass-cli')
 .description("🎨 Interact with Weakpass.com's API through the terminal")
 .version('1.0.0');
 
+loadCommands(program);
 program.parse(process.argv);
